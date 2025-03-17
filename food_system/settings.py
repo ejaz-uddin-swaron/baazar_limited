1a691432e968b71bc83c2f13901b378b4eb1d916
ItsMeSwaron <swaron3214@gmail.com>
2025-03-17 16:34:56 +0600

    again

--- food_system/settings.py
+++ food_system/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-p1&y+a!f9p=4sv4s4ugs#g)#ikk$6f*-7*(5+zc1u-_&9-awsq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
