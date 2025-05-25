bf8a0fdfe8cccd76a1853ac68fda627ae64f9d2f
ItsMeSwaron <swaron3214@gmail.com>
2025-05-25 11:06:43 +0600

    cors moved

--- food_system/settings.py
+++ food_system/settings.py
@@ -72,6 +72,7 @@ SWAGGER_SETTINGS = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -79,7 +80,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
