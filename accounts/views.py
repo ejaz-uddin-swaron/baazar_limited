776b04eb1c64b08d3e61a724cb024795f68a4cf7
ItsMeSwaron <swaron3214@gmail.com>
2025-05-30 00:53:19 +0600

    email link updated

--- accounts/views.py
+++ accounts/views.py
@@ -49,7 +49,7 @@ class UserRegistrationApiView(APIView):
             user = serializer.save()
             token = default_token_generator.make_token(user)
             uid = urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link = f'http://127.0.0.1:8000/client/active/{uid}/{token}/'
+            confirm_link = f'https://baazar-limited-1.onrender.com/client/active/{uid}/{token}/'
             email_subject = 'Confirmation email'
             email_body = render_to_string('confirm_email.html', {'confirm_link':confirm_link})
             email = EmailMultiAlternatives(email_subject, '', to=[user.email])
